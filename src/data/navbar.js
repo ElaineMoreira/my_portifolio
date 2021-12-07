27bcce9e8be6426344a2de9c1d57ee0e24121ace
Elaine Moreira <elainefelipemoreira@gmail.com>
2021-12-07 10:05:27 -0300

    Termina homepage e adiciona @media para vizualizacao

---
+++ src/data/navbar.js
@@ -0,0 +1,19 @@
+import {v4 as uuidv4} from 'uuid';
+
+export const navbar = [
+    {
+        id: uuidv4(),
+        title: "Homepage",
+        url: "/"
+    },
+    {
+        id: uuidv4(),
+        title: "Projetos",
+        url: "projects"
+    },
+    {
+        id: uuidv4(),
+        title: "Contato",
+        url: "contacts"
+    }
+]
