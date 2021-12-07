7c980f6081af3e41ad76ede4faf08c1c50a35cd6
Elaine Moreira <elainefelipemoreira@gmail.com>
2021-12-07 15:32:36 -0300

    Adiciona a pagina de contatos e estiliza ela

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,18 @@
+import { FaPhone, FaEnvelope } from 'react-icons/fa'
 const Contact = () => {
     return (
         <>
+            <section className="contacts">
             <h1>Contato</h1>
+                <ul>
+                    <li>
+                        <a href="tel:31986104739"><FaPhone /> + 55 31 98610-4739</a>
+                    </li>
+                    <li>
+                        <a href="elainefelipemoreira@gmail.com"><FaEnvelope /> elainefelipemoreira@gmail.com</a>
+                    </li>
+                </ul>
+            </section>
         </>
     )
 }
